0ad81c90fca64684cb74f3e8c1292f02ff6527ff
logantestscript <logankumarrasamuthu@gmail.com>
2025-04-04 23:15:03 -0700

    data driven

--- hello_world/test.java
+++ hello_world/test.java
@@ -5,7 +5,7 @@ public class test {
 		System.out.println("Hello World");
 		System.out.println("welcome to testing");
 		System.out.println("welcome to github");
-                System.out.println("Testng");
+        System.out.println("Data driven");
 
 	}
 }
