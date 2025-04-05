18387ac7cc5f6cbae9c35670428a245f7a026efe
logantestscript <logankumarrasamuthu@gmail.com>
2025-04-04 22:24:58 -0700

    Update test.java

--- hello_world/test.java
+++ hello_world/test.java
@@ -4,6 +4,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("welcome to testing");
-		//Empty
+		System.out.println("welcome to github");
 	}
 }
